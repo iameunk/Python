c4282dc819d3fe1a4d86e162fe6991c3e1c135bd
iameunk <eunkyeong24@naver.com>
2022-05-16 23:08:17 +0900

    나머지

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+N = []
+for i in range(10) :
+    n = int(input())
+    N.append(n % 42) // 입력받은 값의 나머지를 배열에 저장
+N = set(arr)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
