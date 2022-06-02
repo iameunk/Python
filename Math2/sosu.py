5e7ca3a3c3498bcdc286fab6255a648ceff23649
iameunk <eunkyeong24@naver.com>
2022-06-02 13:05:35 +0900

    소수 구하기

---
+++ Math2/sosu.py
@@ -0,0 +1,19 @@
+import math
+import sys
+input = sys.stdin.readline
+
+def isPrime(num) :
+    if num == 1 :
+        return False
+
+    sq = int(math.sqrt(num))
+
+    for i in range(2, sq+1) :
+        if num % i == 0 :
+            return False
+    return True
+
+m, n = map(int, input().split())
+for i in range(m, n+1) :
+    if isPrime(i) :
+        print(i)
